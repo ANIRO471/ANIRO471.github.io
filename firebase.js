0ece5102e2f40eb7aff39c0096120728c36a41aa
ANIRO471 <filhoaniro471@gmail.com>
2025-06-28 09:11:50 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,13 @@
 // firebase.js
-// Substitua os valores abaixo pelo SEU config do Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyDaZHDJh8Jw4U1-QT2TYWAOxk3Xy57Sekk",
-  authDomain: "YOUR_AUTH_DOMAIN",
+  authDomain: "testgithub-d13a2.firebaseapp.com",
   projectId: "testgithub-d13a2",
-  storageBucket: "YOUR_STORAGE_BUCKET",
+  storageBucket: "testgithub-d13a2.appspot.com",
   messagingSenderId: "225764043125",
   appId: "1:225764043125:web:d343399c3b3aa5b14a4b3c"
 };
 
-// Inicializa Firebase
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
